2caee2912e6b758b844e7848910c561338368cd2
hapreanmanuel <hapreanmanuel@gmail.com>
2018-02-13 14:05:10 +0000

    Removed unused properties

--- src/main/java/ro/msg/learning/shop/utility/converters/CsvHttpMessageConverter.java
+++ src/main/java/ro/msg/learning/shop/utility/converters/CsvHttpMessageConverter.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import ro.msg.learning.shop.exceptions.CsvConvertionException;
 
 import java.io.IOException;
