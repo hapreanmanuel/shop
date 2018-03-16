4106be739135efd521ec9352301d326979aa3435
hapreanmanuel <hapreanmanuel@gmail.com>
2018-03-16 09:19:26 +0000

    Reworked 'Calculate Revenues' impl Added 'index.html' and ViewController

--- src/main/java/ro/msg/learning/shop/repository/OrderRepository.java
+++ src/main/java/ro/msg/learning/shop/repository/OrderRepository.java
@@ -6,5 +6,5 @@ import ro.msg.learning.shop.domain.Order;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-    List<Order> findByRevenued(boolean revenued);
+    List<Order> findByRevenuedAndStatus(boolean revenued, Order.Status status);
 }
